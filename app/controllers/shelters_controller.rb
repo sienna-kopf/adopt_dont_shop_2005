d694200150612f12e800498c5e42160d169b7e6d
Sienna Kopf <princess.kopf@gmail.com>
2020-07-01 20:26:48 -0600

    Add functionality and test for User Story 4

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -6,4 +6,17 @@ class SheltersController < ApplicationController
   def show
     @shelter = Shelter.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    Shelter.create(shelter_params)
+    redirect_to "/shelters"
+  end
+
+  private
+  def shelter_params
+    params.permit(:name, :address, :city, :state, :zip)
+  end
 end
