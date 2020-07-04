19fc0ab5f8a1d10ae89a17aeeda5e966e477705c
Sienna Kopf <princess.kopf@gmail.com>
2020-07-04 17:24:04 -0600

    Add funcitonality and tests for each scenario possible in user story 15

---
+++ spec/features/usability/pet_update_from_shelters_pets_index_page_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "Update a pet from pet index page" do
+  it "can follow a link from pet index page to a form to update the pet" do
+    shelter = Shelter.create(name: "Foothills Animal Shelter",
+                               address: "580 McIntyre St.",
+                               city: "Golden",
+                               state: "Colorado",
+                               zip: 80401)
+    pet = Pet.create!(image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTUFogxQ6ah2P7oByKgOxr9F8fMZ7lPuQCVlw&usqp=CAU",
+              name: "Frankie",
+              description: "Used to belong to a professional rock climber. Loves bath time!",
+              approximate_age: 7,
+              sex: "male",
+              adoptability: true,
+              shelter: shelter)
+    visit "/shelters/#{shelter.id}/pets"
+
+    click_on "Update #{pet.name}"
+
+    expect(current_path).to eq("/pets/#{pet.id}/edit")
+  end
+end
