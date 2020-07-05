c1fe90ce3b7c24332ba5f6a6407ebcef54e34c50
Sienna Kopf <princess.kopf@gmail.com>
2020-07-05 10:08:22 -0600

    Add link assertions for welcome page

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -7,4 +7,20 @@ RSpec.describe "Welcome page", type: :feature do
 
     expect(page).to have_content("Adopt. Don't Shop.")
   end
+
+  it "can link to shelter index page" do
+    visit '/'
+
+    click_on "Shelters"
+
+    expect(current_path).to eq("/shelters")
+  end
+
+  it "can link to pets index page" do
+    visit '/'
+
+    click_on "Pets"
+
+    expect(current_path).to eq("/pets")
+  end
 end
